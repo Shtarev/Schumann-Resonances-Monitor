37b645df4e3dc504d1c6ec48d2731f4b2f00077f
Andrey Shtarev <54120707+Shtarev@users.noreply.github.com>
2020-09-12 11:40:38 +0200

    Update schumann_robot.php

--- schumann_robot.php
+++ schumann_robot.php
@@ -1,13 +1,10 @@
 <?php
-	define('ROOT_DIR', str_replace('\\', '/', dirname(__FILE__)));  
-	// создаем директорию в которую сохранить изображение
+	define('ROOT_DIR', str_replace('\\', '/', dirname(__FILE__)));
 	if(!is_dir(ROOT_DIR.'/shumann_img')){
 		mkdir(ROOT_DIR.'/shumann_img');
 	}
-	// ссылка где на сайте Томского НИИ находится изображение
 	$link = "http://sosrff.tsu.ru/new/shm.jpg";
-	// куда на сервере грузить фото под именем в виде даты
 	$deport = ROOT_DIR."/shumann_img/".date( "Y.m.d_H.i" ).".jpg";
-	$file = file_get_contents($link); // парсим фото в строку
-	file_put_contents($deport, $file); // пкркводим фото из строки в файл
+	$file = file_get_contents($link);
+	file_put_contents($deport, $file);
 ?>
